0ea5d413269b56d22af219f1feff0a6ce0d80c1f
Eliseous-freelance <96338130+Eliseous-freelance@users.noreply.github.com>
2022-02-14 18:32:23 +0000

    Initial Commit

---
+++ crypto-sapp/crypto_sappApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_sappApp.swift
+//  crypto-sapp
+//
+//  Created by Eliseo Dalle Crode on 14/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct crypto_sappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
